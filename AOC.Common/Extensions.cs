d7d5b102b90311fc2f3a8996d7a336c19c53dea2
Erik <erik.grundy@gmail.com>
2022-12-02 01:19:40 +1300

    First commit! Day 1 done!

---
+++ AOC.Common/Extensions.cs
@@ -0,0 +1,29 @@
+﻿namespace AOC.Common;
+
+public static class Extensions
+{
+    public static IEnumerable<IEnumerable<T>> SplitBy<T>(
+        this IEnumerable<T> input,
+        Func<T, bool> pred)
+    {
+        // List<IEnumerable<T>> result = new();
+        // while (input.Any())
+        // {
+        //     result.Add(input.TakeWhile(x => !pred(x)));
+        //     Console.WriteLine($"{result.Count}, {result.Last().Count()}");
+        //     input = input.SkipWhile(x => !pred(x)).Skip(1);
+        // }
+        // return result;
+        List<IEnumerable<T>> result = new();
+        List<T> current = new();
+        foreach (var x in input)
+            if (pred(x))
+                current.Add(x);
+            else if (current.Any())
+            {
+                result.Add(current);
+                current = new();
+            }
+        return result;
+    }
+}
---
+++ Day01/Day01.cs
@@ -0,0 +1,20 @@
+﻿global using Input = System.Collections.Generic.IEnumerable<int>;
+
+using AOC.Common;
+
+var input = Parse(File.ReadAllLines("inputs/01.txt"));
+Console.WriteLine($"part 1: {Part1(input)}");
+Console.WriteLine($"part 2: {Part2(input)}");
+
+static Input Parse(string[] lines) => lines
+    .SplitBy(line => line != "")
+    .Select(ls => ls.Select(int.Parse).Sum());
+
+static int Part1(Input counts) => counts.Max();
+
+static int Part2(Input counts)
+{
+    var copy = counts.ToList();
+    copy.Sort((e1, e2) => e2.CompareTo(e1));
+    return copy.Take(3).Sum();
+}
