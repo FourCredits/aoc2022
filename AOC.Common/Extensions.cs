aa001c7403ead90edcef957411eed866d29549f7
Erik <erik.grundy@gmail.com>
2022-12-02 01:28:26 +1300

    Remove unnecessary comment

--- AOC.Common/Extensions.cs
+++ AOC.Common/Extensions.cs
@@ -6,14 +6,6 @@ public static class Extensions
         this IEnumerable<T> input,
         Func<T, bool> pred)
     {
-        // List<IEnumerable<T>> result = new();
-        // while (input.Any())
-        // {
-        //     result.Add(input.TakeWhile(x => !pred(x)));
-        //     Console.WriteLine($"{result.Count}, {result.Last().Count()}");
-        //     input = input.SkipWhile(x => !pred(x)).Skip(1);
-        // }
-        // return result;
         List<IEnumerable<T>> result = new();
         List<T> current = new();
         foreach (var x in input)
