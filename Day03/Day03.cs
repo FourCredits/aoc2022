dfbc0313c6b17e0865f375a32d4f117b4f64353f
Erik <erik.grundy@gmail.com>
2022-12-03 23:21:19 +1300

    refactor day 3

--- Day03/Day03.cs
+++ Day03/Day03.cs
@@ -1,29 +1,18 @@
 ﻿var lines = File.ReadAllLines("inputs/03.txt");
-var total = 0;
-foreach (var line in lines)
-{
-    var length = line.Length;
-    var firstCompartment = line.Take(length / 2);
-    var secondCompartment = line.Skip(length / 2);
-    var commonItems = firstCompartment.Intersect(secondCompartment);
-    foreach (var commonItem in commonItems)
-    {
-        total += char.IsLower(commonItem)
-            ? commonItem - 'a' + 1
-            : commonItem - 'A' + 27;
-    }
-}
-Console.WriteLine($"part 1: {total}");
-var total2 = 0;
-foreach (var group in lines.Chunk(3))
-{
-    var badge = group switch
-    {
-        [var a, var b, var c] => a.Intersect(b).Intersect(c).First(),
-        _ => throw new IOException("this should be impossible"),
-    };
-    total2 += char.IsLower(badge)
-        ? badge - 'a' + 1
-        : badge - 'A' + 27;
-}
-Console.WriteLine($"part 2: {total2}");
+Console.WriteLine($"part 1: {Part1(lines)}");
+Console.WriteLine($"part 2: {Part2(lines)}");
+
+static int Part1(string[] rucksacks) => rucksacks
+    .SelectMany(r => r.Take(r.Length / 2).Intersect(r.Skip(r.Length / 2)))
+    .Select(Priority)
+    .Sum();
+
+static int Part2(string[] rucksacks) => rucksacks
+    .Chunk(3)
+    .Select(g => Priority(Intersection(g).First()))
+    .Sum();
+
+static IEnumerable<char> Intersection(IEnumerable<IEnumerable<char>> colls) =>
+    colls.Aggregate((a, b) => a.Intersect(b));
+
+static int Priority(char c) => char.IsLower(c) ? c - 'a' + 1 : c - 'A' + 27;
