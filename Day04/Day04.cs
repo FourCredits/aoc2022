5a46a1a8f51a57294a0eec855b102342bccbb10a
Erik <erik.grundy@gmail.com>
2022-12-04 21:19:01 +1300

    Just thought of a better name

--- Day04/Day04.cs
+++ Day04/Day04.cs
@@ -6,11 +6,11 @@ Console.WriteLine($"part 2: {part2}");
 
 (T2, T1) Flip<T1, T2>((T1 first, T2 second) pair) => (pair.second, pair.first);
 
-bool Contains(((int low, int high) first, (int low, int high) second) pair) =>
-    pair.first.low >= pair.second.low && pair.first.high <= pair.second.high;
+bool Contains(((int low, int high) elf1, (int low, int high) elf2) pair) =>
+    pair.elf1.low >= pair.elf2.low && pair.elf1.high <= pair.elf2.high;
 
-bool Overlap(((int low, int high) first, (int low, int high) second) pair) =>
-    pair.first.high >= pair.second.low && pair.first.low <= pair.second.high;
+bool Overlap(((int low, int high) elf1, (int low, int high) elf2) pair) =>
+    pair.elf1.high >= pair.elf2.low && pair.elf1.low <= pair.elf2.high;
 
 ((int, int), (int, int)) Parse(string line) =>
     line.Split(new[] { '-', ',' }).Select(int.Parse).ToArray() switch
