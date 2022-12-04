243fde722a55854e1bec9a86a862b0141a6ae3a0
Erik <erik.grundy@gmail.com>
2022-12-04 18:11:50 +1300

    Made some tweaks inspired by [Dylan Beattie](https://twitter.com/dylanbeattie/status/1599042232681521153?t=dOwiZ4q_WER-rIdN8_x7iQ&s=19)

--- Day03/Day03.cs
+++ Day03/Day03.cs
@@ -2,17 +2,19 @@
 Console.WriteLine($"part 1: {Part1(lines)}");
 Console.WriteLine($"part 2: {Part2(lines)}");
 
-static int Part1(string[] rucksacks) => rucksacks
-    .SelectMany(r => r.Take(r.Length / 2).Intersect(r.Skip(r.Length / 2)))
-    .Select(Priority)
-    .Sum();
+int Part1(string[] rucksacks) => rucksacks
+    .Select(r => FindCommonElement(Bisect(r)))
+    .Sum(Priority);
 
-static int Part2(string[] rucksacks) => rucksacks
+int Part2(string[] rucksacks) => rucksacks
     .Chunk(3)
-    .Select(g => Priority(Intersection(g).First()))
-    .Sum();
+    .Select(FindCommonElement)
+    .Sum(Priority);
 
-static IEnumerable<char> Intersection(IEnumerable<IEnumerable<char>> colls) =>
-    colls.Aggregate((a, b) => a.Intersect(b));
+string[] Bisect(string s) =>
+    new[] { s[..(s.Length / 2)], s[(s.Length / 2)..] };
 
-static int Priority(char c) => char.IsLower(c) ? c - 'a' + 1 : c - 'A' + 27;
+char FindCommonElement(IEnumerable<IEnumerable<char>> colls) =>
+    colls.Aggregate((a, b) => a.Intersect(b)).Single();
+
+int Priority(char c) => char.IsLower(c) ? c - 'a' + 1 : c - 'A' + 27;
