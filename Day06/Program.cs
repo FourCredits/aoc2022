4a74769b0741bd9e3ea44032280fe26c294c3d89
Erik <erik.grundy@gmail.com>
2022-12-07 01:20:01 +1300

    Day 6: done in like 5 minutes. What was meant to be hard about this?

---
+++ Day06/Program.cs
@@ -0,0 +1,12 @@
+﻿var line = File.ReadAllLines("inputs/06.txt").Single();
+// 1911 is too low
+Console.WriteLine($"part 1: {FindNDistinctCharacters(line, 4)}");
+Console.WriteLine($"part 2: {FindNDistinctCharacters(line, 14)}");
+
+static int? FindNDistinctCharacters(string s, int n)
+{
+    for (var i = 0; i < s.Length; ++i)
+        if (s[i..(i + n)].Distinct().Count() == n)
+            return i + n;
+    return null;
+}
