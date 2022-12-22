54994f5a4f07a2f79b60ad9ff8f8d3b73a4dd4f0
Erik Grundy <Erik.Grundy@alstefgroup.com>
2022-12-22 15:19:18 +1300

    Day 15 complete

---
+++ AOC.Common/P2.cs
@@ -0,0 +1,31 @@
+namespace AOC.Common;
+
+public record struct P2(int X, int Y)
+{
+    public static P2 operator +(P2 p1, P2 p2) =>
+        new(p1.X + p2.X, p1.Y + p2.Y);
+
+    public static P2 operator -(P2 p1, P2 p2) =>
+        new(p1.X - p2.X, p1.Y - p2.Y);
+
+    public static P2 operator *(P2 vector, int scalar) =>
+        new(vector.X * scalar, vector.Y * scalar);
+
+    public static int Manhattan(P2 p1, P2 p2) =>
+        int.Abs(p1.X - p2.X) + int.Abs(p1.Y - p2.Y);
+
+    public static IEnumerable<P2> ReverseManhattan(P2 point, int distance) =>
+        Enumerable.Range(0, distance).SelectMany(i => new[]
+            {
+                point + (new P2(1, -1) * i) + new P2(distance, 0),
+                point + (new P2(-1, 1) * i) + new P2(-distance, 0),
+                point + (new P2(-1, -1) * i) + new P2(0, distance),
+                point + (new P2(1, 1) * i) + new P2(0, -distance),
+            });
+
+    public bool InBounds((P2 topLeft, P2 bottomRight) bounds) =>
+        X >= bounds.topLeft.X &&
+        X <= bounds.bottomRight.X &&
+        Y >= bounds.topLeft.Y &&
+        Y <= bounds.bottomRight.Y;
+}
--- Day15/Day15.cs
+++ Day15/Day15.cs
@@ -1,33 +1,37 @@
 ﻿using AOC.Common;
 
-var practice = false;
-var lines =
-    File.ReadAllLines(practice ? "inputs/practice.txt" : "inputs/15.txt");
-var y = practice ? 10 : 2_000_000;
-HashSet<(int, int)> beacons = new();
-Dictionary<(int x, int y), int> sensors = new(lines
+var lines = File.ReadAllLines("inputs/15.txt");
+var y = 2_000_000;
+HashSet<P2> beacons = new();
+Dictionary<P2, int> sensors = new(lines
     .Select(line =>
         {
-            var (sx, sy, bx, by) = ParseLine(line);
-            var distance = Manhattan((sx, sy), (bx, by));
-            _ = beacons.Add((bx, by));
-            return new KeyValuePair<(int, int), int>((sx, sy), distance);
+            var (sensor, beacon) = ParseLine(line);
+            var distance = P2.Manhattan(sensor, beacon);
+            _ = beacons.Add(beacon);
+            return new KeyValuePair<P2, int>(sensor, distance);
         }));
-var minBound = sensors.Select(kv => kv.Key.x - kv.Value).Min();
-var maxBound = sensors.Select(kv => kv.Key.x + kv.Value).Max();
+var minBound = sensors.Select(kv => kv.Key.X - kv.Value).Min();
+var maxBound = sensors.Select(kv => kv.Key.X + kv.Value).Max();
 var part1 = Enumerable
     .Range(minBound, maxBound - minBound + 1)
-    .Select(x => (x, y))
+    .Select(x => new P2(x, y))
     .Count(p => !beacons.Contains(p) &&
         !sensors.ContainsKey(p) &&
-        sensors.Any(kv => Manhattan(kv.Key, p) <= kv.Value));
+        sensors.Any(kv => P2.Manhattan(kv.Key, p) <= kv.Value));
 Console.WriteLine($"part 1: {part1}"); // 5564017
 
-static int Manhattan((int x, int y) p1, (int x, int y) p2) =>
-    int.Abs(p1.x - p2.x) + int.Abs(p1.y - p2.y);
+var searchSize = 4_000_000;
+var (distressX, distressY) = sensors
+    .Select(kv => P2.ReverseManhattan(kv.Key, kv.Value + 1))
+    .Aggregate((s1, s2) => s1.Union(s2))
+    .Single(p => p.InBounds((new(0, 0), new(searchSize, searchSize))) &&
+        sensors.All(kv => P2.Manhattan(kv.Key, p) > kv.Value));
+var part2 = ((ulong)distressX * (ulong)searchSize) + (ulong)distressY;
+Console.WriteLine($"part 2: {part2}"); // 11558423398893
 
-static (int, int, int, int) ParseLine(string s) => Parse.ManyInts(s) switch
+static (P2, P2) ParseLine(string s) => Parse.ManyInts(s) switch
 {
-    [var i1, var i2, var i3, var i4] => (i1, i2, i3, i4),
+    [var i1, var i2, var i3, var i4] => (new(i1, i2), new(i3, i4)),
     _ => throw new ArgumentException($"wrong number of ints"),
 };
